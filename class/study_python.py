3b509ab432b10079768f1b05e51aae22b6aed246
GGCPP89S51 <92510306+GGCPP89S51@users.noreply.github.com>
2023-11-23 09:38:34 +0800

    Update study_python.py

--- class/study_python.py
+++ class/study_python.py
@@ -1,19 +1,26 @@
-class graphics:
-    def __init__(self,square_side,length,weidth,redius) -> None:
-        self.square_side,self.length,self.weidth,self.redius = square_side,length,weidth,redius
+class Graphics:
+    def __init__(self, square_side=None, length=None, width=None, radius=None):
+        self.square_side, self.length, self.width, self.radius = square_side, length, width, radius
     
-    def getSquareArea(self) :
-        Square = self.square_side * self.square_side
-        return square
+    def getSquareArea(self):
+        square_area = self.square_side * self.square_side
+        return square_area
 
-    def getRectangleArea(self) :
-        Rectangle = self.length *self.weidth
-        return Rectangle
+    def getRectangleArea(self):
+        rectangle_area = self.length * self.width
+        return rectangle_area
 
-    def getCircleArea(self) :
-        Circle = self.redius *self.redius *3.14
-        return Circle
+    def getCircleArea(self):
+        circle_area = self.radius * self.radius * 3.14
+        return circle_area
 
 def main():
-    Graphics = graphics
+    graphics_object = Graphics(5, 10, 5, 6)
+    print(graphics_object.getSquareArea())
+    print(graphics_object.getRectangleArea())
+    print(graphics_object.getCircleArea())
+
+if __name__ =='__main__':
+    main()
+
     
