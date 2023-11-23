13157192decccf203af02315ab35b8c702a45e94
GGCPP89S51 <92510306+GGCPP89S51@users.noreply.github.com>
2023-11-23 11:33:23 +0800

    Update homework6.py

--- class/homework6.py
+++ class/homework6.py
@@ -1,10 +1,13 @@
 class Employee :
     def __init__(self,emp_name=None, emp_id=None, emp_salary=None,emp_department=None) :
         self.emp_id , self.emp_name ,self.emp_salary ,self.emp_deparment = emp_id , emp_name , emp_salary , emp_department
+
     def assign_department(self,new_department):
         self.emp_deparment = new_department
+
     def print_employee_details(self) :
         print(self.emp_id,self.emp_name,self.emp_salary,self.emp_deparment)
+
     def calculate_emp_salary(self,hour = 0):
         hour_salary = self.emp_salary/50
         if hour < 50 :
